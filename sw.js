6983d1b92443d0021461f0ae5a7ca6a53dbcb6ca
manojmallah58-source <manojmallah58@gmail.com>
2026-07-15 21:54:04 +0530

    Add caching for assets in service worker
    Implement caching strategy for assets in service worker

--- sw.js
+++ sw.js
@@ -1,4 +1,17 @@
-// sw.js (Service Worker for PWA)
-self.addEventListener('install', () => {
-    console.log('QRify PWA ready');
+const CACHE_NAME = 'qr-pro-v1';
+const assets = [
+  '/',
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  'https://unpkg.com/qr-code-styling@1.5.0/lib/qr-code-styling.js',
+  'https://unpkg.com/lucide@latest'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
